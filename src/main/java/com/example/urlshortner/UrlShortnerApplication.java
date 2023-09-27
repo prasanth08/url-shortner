7f6c7149909c6dbc9c9ea621572332ec20bfd2ba
Prasanth Omanakuttan <pomanakuttan9642@gmail.com>
2023-09-27 22:40:04 +0530

    Virtual Threads support for each request

--- src/main/java/com/example/urlshortner/UrlShortnerApplication.java
+++ src/main/java/com/example/urlshortner/UrlShortnerApplication.java
@@ -2,12 +2,27 @@ package com.example.urlshortner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @SpringBootApplication
 public class UrlShortnerApplication {
+    private static final Logger LOGGER = Logger.getLogger(UrlShortnerApplication.class.getName());
 
     public static void main(String[] args) {
         SpringApplication.run(UrlShortnerApplication.class, args);
+    }
+
+    @Bean
+    TomcatProtocolHandlerCustomizer<?> protocolHandlerVirtualThreadExecutorCustomizer() {
+        return protocolHandler -> {
+            LOGGER.log(Level.ALL,"Configuring {0} to use VirtualThreadPerTaskExecutor",protocolHandler);
+            protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
+        };
     }
 
 }
