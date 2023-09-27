3d4c0034783dc7248efaf25ef15b24e322e50fed
Prasanth Omanakuttan <pomanakuttan9642@gmail.com>
2023-09-27 10:31:26 +0530

    Exposed placeholder api | added requests to test

--- src/main/java/com/example/urlshortner/controller/URLShortnerController.java
+++ src/main/java/com/example/urlshortner/controller/URLShortnerController.java
@@ -1,12 +1,11 @@
 package com.example.urlshortner.controller;
 
 
+import com.example.urlshortner.model.ShortenRequest;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("app")
@@ -15,5 +14,12 @@ public class URLShortnerController {
     @GetMapping("redirect/{shortUrl}")
     public ResponseEntity<?> handleRedirectForShortUrl(@PathVariable String shortUrl){
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+    @PostMapping(value = "/shorten",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<?> shortenUrl(@RequestBody ShortenRequest request){
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }
---
+++ src/main/java/com/example/urlshortner/model/ShortenRequest.java
@@ -0,0 +1,4 @@
+package com.example.urlshortner.model;
+
+public record ShortenRequest(String originalUrl) {
+}
